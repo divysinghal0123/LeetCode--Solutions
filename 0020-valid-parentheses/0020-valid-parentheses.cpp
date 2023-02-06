da4ecfb76bdc03c32996cd2597c7c44830f266ba
Divy Singhal <76985594+divysinghal0123@users.noreply.github.com>
2023-02-06 22:30:58 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (96.72%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n=s.size();
+        stack<char> st;
+        bool ans = true;
+        int i;
+        for(i=0;i<n;i++){
+            if(s[i]=='('||s[i]=='{'||s[i]=='['){
+                st.push(s[i]);
+            }
+            else if(s[i]=='}'){
+                if(!st.empty() && st.top()=='{'){
+                    st.pop();
+                }
+                else{
+                    ans=false;
+                    break;
+                }
+            }
+            else if(s[i]==']'){
+                if(!st.empty() && st.top()=='['){
+                    st.pop();
+                }
+                else{
+                    ans=false;
+                    break;
+                }
+            }
+            else if(s[i]==')'){
+                if(!st.empty() && st.top()=='('){
+                    st.pop();
+                }
+                else{
+                    ans=false;
+                    break;
+                }
+            }
+        }
+        if(!st.empty()){
+            return false;
+        }else{
+            return ans;
+        }
+    }
+};
