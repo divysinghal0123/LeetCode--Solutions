f400e7935d4d44976003c4a48cf562bdc827c94c
Divy Singhal <76985594+divysinghal0123@users.noreply.github.com>
2023-03-29 00:30:47 +0530

    Time: 19 ms (11.14%), Space: 6 MB (70.00%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -4,12 +4,10 @@ public:
         if(n == 0){
             return 0;
         }
-        else if(n == 1){
+        if(n == 1){
             return 1;
-        }
-        else{
+        }else{
             return (fib(n-1)+fib(n-2));
         }
-        
     }
 };
