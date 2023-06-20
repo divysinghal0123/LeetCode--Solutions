ae3999066c2d7ae991e6cf3b1278923e97fa927c
Divy Singhal <76985594+divysinghal0123@users.noreply.github.com>
2023-06-20 12:39:24 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (43.84%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+        int i;
+        for(i=0;i<n;i++){
+            ans.push_back(nums[i]);
+        }
+        sort(nums.begin(),nums.end());
+        for(i=0;i<n;i++){
+            nums[i] = nums[i];
+        }
+    }
+};
