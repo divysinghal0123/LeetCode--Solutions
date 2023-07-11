b2c9afa34e6c97559cc0ba5501f9a8f7956e8509
Divy Singhal <76985594+divysinghal0123@users.noreply.github.com>
2023-07-12 00:06:39 +0530

    Time: 39 ms (8.62%), Space: 22.3 MB (77.65%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& arr) {
-        if(arr.size()==1) return arr[0];
-        if(arr.size()==0) return 0;
-        int no =0; 
-        int i =0; // IST ELEMENT OF THE PAIR
-        int j =1; // 2ND ELEMENT OF THE PAIR
-        while(i<arr.size()){
-            if(arr[j]!=arr[i]){ // CHECK THE PAIR 
-                no = arr[i];  // IF NOT VALID THE 1ST IS THE NO. STORE IT 
-                break;   /// DONT FORGET TO BREAK
-            } 
-            i +=2;   // MOVE YOUR WINDOW 2 STEP AHEAD
-            j +=2;    // MOVE YOUR WINDOW 2 STEP
+    int singleNonDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        if( n == 0) return 0;
+        if( n == 1) return nums[0];
+        int count = 0;
+        int i = 0;
+        int j = 1;
+        while(i < n){
+            if(nums[i] != nums[j]){
+                count = nums[i];
+                break;
+            }
+            i+=2;
+            j+=2;
+            
         }
-        return no;  // AND YOU WILL GET THE ANS
+        return count;
     }
 };
