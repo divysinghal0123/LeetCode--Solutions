1d36b5e80ecb6b433ef89f77a51ae7426dcf85fa
Divy Singhal <76985594+divysinghal0123@users.noreply.github.com>
2023-07-12 00:21:10 +0530

    Time: 40 ms (8.19%), Space: 22.1 MB (99.62%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -3,19 +3,18 @@ public:
     int singleNonDuplicate(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int n = nums.size();
-        if( n == 0) return 0;
-        if( n == 1) return nums[0];
+        if(n==0) return 0;
+        if(n==1) return nums[0];
         int count = 0;
         int i = 0;
         int j = 1;
-        while(i < n){
+        while(i<n){
             if(nums[i] != nums[j]){
                 count = nums[i];
                 break;
             }
             i+=2;
             j+=2;
-            
         }
         return count;
     }
